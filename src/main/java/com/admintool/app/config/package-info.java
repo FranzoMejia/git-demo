20522bedd2e48a3ee46adb3f342bc19e1901bdd8
Oscar Mejia <camecac35@gmail.com>
2022-04-29 10:18:57 -0500

    Initial commit

---
+++ src/main/java/com/admintool/app/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.admintool.app.config;
---
+++ src/main/java/com/admintool/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.admintool.app.repository;
+
+import com.admintool.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
